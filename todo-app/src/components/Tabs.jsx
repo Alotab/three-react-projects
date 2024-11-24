e1671a0f6a46e5b2b233b3bae043fff065b67e38
Albert Osei Frimpong <alotab@yahoo.com>
2024-11-24 03:47:03 -0800

    add TodoCard, TodoList, TodoInput files

---
+++ todo-app/src/components/Tabs.jsx
@@ -0,0 +1,30 @@
+
+
+const Tabs = (props) => {
+  const { todos } = props
+  const tabs = ['All', 'Open', 'Completed']
+  return (
+    <nav className="tab-container">
+
+      {
+        tabs.map((tab, tabIndex) => {
+          const numOfTask = tab === 'All' ? 
+                todos.length :
+                tab === 'Open' ?
+                    todos.filter(val => !val.complete).length :
+                    todos.filter(val => val.complete).length
+
+
+          return (
+            <button className="tab-button" key={tabIndex}>
+              <h4>{tab} <span>({ numOfTask })</span></h4>
+            </button>
+          )
+        })
+      }
+
+    </nav>
+  )
+}
+
+export default Tabs
---
+++ todo-app/src/components/TodoInput.jsx
@@ -0,0 +1,12 @@
+
+
+
+
+
+const TodoInput = () => {
+  return (
+    <div>TodoInput</div>
+  )
+}
+
+export default TodoInput;
